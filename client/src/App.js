4322f09c10cf564602929560c9cd6535007a4edc
Vansh0100 <vanshjaiswal945@gmail.com>
2022-12-23 21:50:46 +0530

    Frontend Setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl text-center text-red-600 italic'>Welcome to E-Library</h1>
+    </>
+  );
+}
+
+export default App;
